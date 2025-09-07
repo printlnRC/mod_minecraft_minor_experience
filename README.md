a798b499b3c8fc17f43ea4ae038b1d590988844a
printlnRC <samtao.coulet@gmail.com>
2025-09-07 16:39:33 +0200

    feat: correction mod id

--- README.md
+++
@@ -1,2 +0,0 @@
-# mod_minecraft_minor_experience
-Hello this repository is a small minecraft mod which aims to add tools and ores, it is my first minecraft mod
--- src/main/java/net/println/drillmod/Config.java
+++ src/main/java/net/println/drillmod/Config.java
@@ -1,9 +1,9 @@
-package net.println.mod;
+package net.println.drillmod;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,8 +14,9 @@ import java.util.stream.Collectors;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
-@Mod.EventBusSubscriber(modid = DrillMod.MO_DID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Config {
+@Mod.EventBusSubscriber(modid = DrillMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config
+{
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
@@ -42,12 +43,14 @@ public class Config {
     public static String magicNumberIntroduction;
     public static Set<Item> items;
 
-    private static boolean validateItemName(final Object obj) {
+    private static boolean validateItemName(final Object obj)
+    {
         return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(ResourceLocation.tryParse(itemName));
     }
 
     @SubscribeEvent
-    static void onLoad(final ModConfigEvent event) {
+    static void onLoad(final ModConfigEvent event)
+    {
         logDirtBlock = LOG_DIRT_BLOCK.get();
         magicNumber = MAGIC_NUMBER.get();
         magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();

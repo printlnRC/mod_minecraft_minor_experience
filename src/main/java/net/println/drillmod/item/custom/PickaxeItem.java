6a96951eaf0a8c2fc8d6478a47a240ebbbfa7534
printlnRC <samtao.coulet@gmail.com>
2025-09-12 21:17:38 +0200

    feat/test-add-new-pickaxe

---
+++ src/main/java/net/println/drillmod/item/custom/PickaxeItem.java
@@ -0,0 +1,9 @@
+package net.println.drillmod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class PickaxeItem extends Item {
+    public PickaxeItem(Properties pProperties) {
+        super(pProperties);
+    }
+}
